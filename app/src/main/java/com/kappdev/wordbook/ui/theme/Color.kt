66a8145a0323901fd3c3e937f7849ac57a58ac30
Khotych Mykola <kola13r@gmail.com>
2024-02-27 17:11:38 +0200

    Colors and Theme updated

--- app/src/main/java/com/kappdev/wordbook/ui/theme/Color.kt
+++ app/src/main/java/com/kappdev/wordbook/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.kappdev.wordbook.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SkyBlue = Color(0xFF3B98E2)
+val Charcoal = Color(0xFF343434)
+val Graphite = Color(0xFF363636)
+val SilverSlate = Color(0xFF918C8C)
+val Ashen = Color(0xFFBFBEBE)
+val LinenWhite = Color(0xFFF0E9E0)
+val FrostWhite = Color(0xFFFCFEFD)
--- app/src/main/java/com/kappdev/wordbook/ui/theme/Theme.kt
+++ app/src/main/java/com/kappdev/wordbook/ui/theme/Theme.kt
@@ -10,38 +10,38 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = SkyBlue,
+    onPrimary = Color.White,
+
+    background = Charcoal,
+    onBackground = Ashen,
+
+    surface = Color.Black,
+    onSurface = FrostWhite,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = SkyBlue,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
+    background = LinenWhite,
+    onBackground = SilverSlate,
+
+    surface = Color.White,
+    onSurface = Graphite,
 )
 
 @Composable
 fun WordBookTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
